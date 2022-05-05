8b45cefbcf31206e81ced1f7de672cb11ef01706
Mmargar001 <martin27113456@gmail.com>
2022-05-05 13:28:43 +0200

    Modificacion de la clase Main

---
+++ src/proyectocomunalumnosgit/ProyectoComunAlumnosGit.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectocomunalumnosgit;
+
+/**
+ *
+ * @author Sonia
+ */
+public class ProyectoComunAlumnosGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Probando Github con múltiples colaboradores");
+      
+        System.out.println("Método de Marta");
+    }
+
+}
+ 
+ 
