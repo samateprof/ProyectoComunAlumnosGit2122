ed4cb6bd00ac45170dc82d06f3a33585f15a18cd
WARMAN113 <103250907+WARMAN113@users.noreply.github.com>
2022-05-05 14:00:01 +0200

    Add files via upload

---
+++ ProyectoComunAlumnosGit2122-master/src/proyectocomunalumnosgit/ProyectoComunAlumnosGit.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectocomunalumnosgit;
+
+/**
+ *
+ * @author Sonia
+ */
+public class ProyectoComunAlumnosGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Probando Github con múltiples colaboradores");
+      
+        System.out.println("Método de Marta");
+        
+    }
+    public static void metodoMario (){
+        System.out.println("Método de Mario Blasco");
+    }
+}
+ 
+ 
