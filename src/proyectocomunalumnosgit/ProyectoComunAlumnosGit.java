e451a1c96c6cad1992020aff035681d8d8dc0e57
Almudena <Almudena@DESKTOP-5SJF2SR>
2022-05-06 11:53:07 +0200

    cambiar metodo3

--- src/proyectocomunalumnosgit/ProyectoComunAlumnosGit.java
+++ src/proyectocomunalumnosgit/ProyectoComunAlumnosGit.java
@@ -22,9 +22,8 @@ public class ProyectoComunAlumnosGit {
     
     
     
-      public static String almudena(String nombre){
-           nombre= "Almudena";
-           return nombre;
+      public static void almudena(){
+           System.out.println("Almudena");
     }
 
 
