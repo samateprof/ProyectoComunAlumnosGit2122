e368655c52c48d6ca436655101f0d9f8e161d3dd
Almudena <Almudena@DESKTOP-5SJF2SR>
2022-05-06 11:37:12 +0200

    cambiar nombre

--- src/proyectocomunalumnosgit/ProyectoComunAlumnosGit.java
+++ src/proyectocomunalumnosgit/ProyectoComunAlumnosGit.java
@@ -17,7 +17,7 @@ public class ProyectoComunAlumnosGit {
     public static void main(String[] args) {
         System.out.println("Probando Github con múltiples colaboradores");
       
-        System.out.println("Método de Marta");
+        System.out.println("Método de Almudena");
     }
     
     
